8b88914c08334be0f166adacca9bed620386f729
daimuyang <734583898@qq.com>
2018-03-08 19:19:18 +0800

    change axios-base-url

--- src/api/api.js
+++ src/api/api.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-let base = 'http://120.79.232.154:3000';
+// let base = 'http://120.79.232.154:3000'; //此接口和webpack dev-server相同
+let base = '';
 
 export const requestLogin = params => {                //用户登录请求
     return axios.post(`${base}/api/login`, params).then(res => {
